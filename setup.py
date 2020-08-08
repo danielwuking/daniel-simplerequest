f9eb79fb67712e227c7d1f04949f37f183b8a338
吳冠言 <danielwu@MacBook-Pro.local>
2020-08-08 12:50:35 +0800

    Bump version and enable distributing package.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='daniel_simplerequest',
-      version='0.1',
+      version='1.0',
       description='simple request package based on python standard libraries',
       author='Daniel Wu',
       license='MIT',
