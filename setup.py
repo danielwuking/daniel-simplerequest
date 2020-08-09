ae1abb69c30ef4aaf761fde4ce4750f8eef5a506
吳冠言 <danielwu@MacBook-Pro.local>
2020-08-09 17:12:52 +0800

    Update set.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='daniel_simplerequest',
-      version='1.0',
-      description='simple request package based on python standard libraries',
+      version='1.2',
+      description='Simple request package based on python standard libraries',
       author='Daniel Wu',
       license='MIT',
-      packages=['daniel_simplerequest'],
+      packages=['daniel_simplerequest', 'error', 'request'],
       zip_safe=False)
